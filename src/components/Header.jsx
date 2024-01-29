802cbb81ccf13812a5e8e6555d329bc4e11791a3
a14a19 <anas31197@gmail.com>
2024-01-29 13:29:12 +0530

    hero done, header and footer in progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
     return (
-        <div>Header</div>
+        <header className="z-30 flex justify-between items-center relative w-full">
+            <h2 className="text-5xl font-extrabold">
+                RI5E
+            </h2>
+            <div className="flex gap-20 items-center justify-center text-xl font-semibold">
+                <Link>
+                    About Us
+                </Link>
+                <Link>
+                    Services
+                </Link>
+                <Link>
+                    Careers
+                </Link>
+                <button className="bg-blue-700 px-5 py-3">
+                    Get In Touch
+                </button>
+            </div>
+        </header>
     )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header"
+
+
+
+export default function Hero() {
+    return (
+        <section className="w-full h-screen flex flex-col justify-between items-start relative overflow-hidden pt-6 px-16 pb-32">
+            <Header />
+            <video autoPlay loop muted playsInline className="absolute top-0 bottom-0 right-0 left-0 z-0">
+                <source src="https://6069178.fs1.hubspotusercontent-na1.net/hubfs/6069178/FGYHJ.mp4" type="video/mp4" />
+            </video>
+            <h1 className="z-30 relative font-black text-6xl w-10/12">Elevating Potential, Transforming Teams - Your Catalyst for HR Excellence.</h1>
+        </section>
+    )
+}
+
