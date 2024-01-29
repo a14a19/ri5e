5e4ff72172807a84fde6d59c32e9c71bc4fdd5f5
Anas Ahmed <anas31197@gmail.com>
2024-01-29 20:07:06 +0530

    code merged (#6)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
+import Levelup from "../components/Levelup";
+import Nutshell from "../components/Nutshell";
 
 export default function Home() {
     return (
         <>
             <Hero />
+            <Nutshell />
+            <Levelup />
             <Footer />
         </>
     )
