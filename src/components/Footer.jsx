31468e76dd225ecb77d9f74fe93d51d9eef715dc
Anas Ahmed <anas31197@gmail.com>
2024-01-29 16:10:00 +0530

    Anas 408 29124 (#3)
    * video added
    
    * filter added on video

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,16 @@
 export default function Footer() {
     return (
         <footer className="bg-black px-40 py-20 flex items-start justify-between w-full">
-            <div className="w-[65%]">
+            <div className="w-[65%] flex flex-col">
                 <h2 className="text-5xl font-bold">
                     RI5E
                 </h2>
+                <div className="">
+                    California, US
+                </div>
+                <div>
+                    +31(0)202101211
+                </div>
             </div>
             <div className="w-[35%]">
                 <h2 className="text-5xl font-black">
