9becbb444a5ffc65273e610678d4e0ac3848fdb5
Anas Ahmed <anas31197@gmail.com>
2024-01-29 20:00:41 +0530

    Levelup and Nutshell Added! (#4)
    Co-authored-by: GoatSKR <sitesh.raulo.pj@gmail.com>

---
+++ src/components/Nutshell.jsx
@@ -0,0 +1,41 @@
+export default function Nutshell() {
+    return (
+        <div className="py-[10%] ">
+            <div className="text-[45px] lg:text-[55px] font-sans font-extrabold mb-[3%] lg:mb-[6%]	px-[5%] lg:px-[8%] leading-tight">
+                <h1>
+                    Ri5e in a Nutshell
+                </h1>
+            </div>
+            <div className="flex  flex-col lg:flex-row  place-items-center">
+                <div className="w-[100%] lg:w-[560%] mb-[4%] lg:pl-[8%] ">
+                    <img className="" src="https://www.wearekeen.com/hs-fs/hubfs/wearekeen-content-214%20(1).jpg?width=2000&height=2000&name=wearekeen-content-214%20(1).jpg"
+                        alt="hr Image" />
+                </div>
+                <div className=" ml-[3%] lg:ml-[7%] text-[18px] lg:text-[22px] mr-[2%] px-[5%] lg:px-[0%] mt-[5%] lg:mt-[0%]">
+                    <p>
+                        Ri5e, founded in the netehrlands in 2018 by three
+                        Dutch Enterprenuers, is on a mission to create the best
+                        and moist fun embedded recruitment company in Europe.
+                        Our Team consists of goal oriented driven,m forward-thinking,
+                        and enthusaistic Talent Partners with extensive experience in
+                        recuritment.
+                    </p>
+                    <p className="mt-[3%]">
+                        At the heart of our operations are our core values #Ri5etoGrow
+                        #Ri5etoLearn, #Ri5etoCare, and #Ri5etoHaveFun. We firmly
+                        believe in nurturing talent and providing them with opportunities
+                        for professional and personal growth. Our unique strength lies
+                        in bringing together a vibrant community of like-minded
+                        individuals who share a passion for continuous learning,
+                        ambition, kindness, and positive vibes.
+                    </p>
+                    <div className=" mt-[13%] lg:mt-[17%]">
+                        <button className="text-[16px] lg:text-[18px] font-bold  bg-[#4353FF] hover:text-blue-500 hover:bg-white px-[22px] py-[15px] lg:px-[35px] lg:py-[22px]">
+                            More About Us
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
