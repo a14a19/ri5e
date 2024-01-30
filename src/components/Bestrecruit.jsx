89c8a74d889ff0272edcf523283bb46faeef1cfc
GoatSKR <sitesh.raulo.pj@gmail.com>
2024-01-30 18:12:47 +0530

    recruit, stand, worktogether, offices components added!

---
+++ src/components/Bestrecruit.jsx
@@ -0,0 +1,34 @@
+export default function Bestrecruit() {
+    return (
+        <div className="pt-[10%] ">
+            <div className="text-[45px] lg:text-[55px] font-sans font-extrabold mb-[5%] lg:mb-[6%]	px-[5%] lg:px-[8%] leading-tight text-center">
+                <h1>
+                    "We want to be the best and most fun embedded recruitment company in Europe"
+                </h1>
+            </div>
+            <div className="grid gap-y-12  lg:grid-cols-4 grid-cols-2 px-[10%] mt-[20%] md:mt-[5%] ">
+
+                
+                    <div className=" flex flex-col items-center  ">
+                        <img className="w-[40%]  " src="https://www.wearekeen.com/hubfs/WeAreKeen_May_2021/images/keen-to-grow.svg" />
+                        <p className="mt-[4%] text-center md:text-[25px] text-[15px]"> #grow</p>
+                    </div>
+                    <div className=" flex flex-col items-center ">
+                        <img className="w-[40%] " src="https://www.wearekeen.com/hubfs/WeAreKeen_May_2021/images/keen-to-learn.svg" />
+                        <p className="mt-[4%] text-center md:text-[25px] text-[15px]"> #learn</p>
+                    </div>
+               
+              
+                    <div className=" flex flex-col items-center ">
+                        <img className="w-[40%]  " src="https://www.wearekeen.com/hubfs/WeAreKeen_May_2021/images/keen-to-share.svg" />
+                        <p className="mt-[4%] text-center md:text-[25px] text-[15px]"> #care</p>
+                    </div>
+                    <div className=" flex flex-col items-center ">
+                        <img className="w-[40%]  " src="https://www.wearekeen.com/hubfs/WeAreKeen_May_2021/images/Group%20135.svg" />
+                        <p className="mt-[4%] text-center md:text-[25px] text-[15px]"> #havefun</p>
+                    </div>
+                
+            </div>
+        </div>
+    )
+}
--- src/components/Levelup.jsx
+++ src/components/Levelup.jsx
@@ -1,14 +1,7 @@
 export default function Levelup() {
     return (
         <div >
-            <div className="py-[16%] bg-black bg-cover bg-center"
-                style={window.innerWidth >= 1024 ? {
-                    backgroundImage: `url("https://www.wearekeen.com/hubfs/WeAreKeen_May_2021/images/europe-desktop.svg")`,
-                    backgroundRepeat: 'no-repeat',
-                    width: '100%',
-                    height: '100%',
-                    backgroundPosition: 'center'
-                } : {}} >
+            <div className="py-[16%] bg-black bg-cover bg-center lg:bg-[url('https://www.wearekeen.com/hubfs/WeAreKeen_May_2021/images/europe-desktop.svg')]">
                 <div className=" px-[8%]  text-left lg:w-2/3 ">
                     <div className="text-[45px] lg:text-[55px] font-sans font-extrabold mb-[6%] 	">
                         <h1 className="leading-tight">
@@ -17,14 +10,18 @@ export default function Levelup() {
                     </div>
                     <div className=" text-[18px] lg:text-[22px] mr-[2%] mt-[15%] lg:mt-[0%]">
                         <p>
-                            Hello! Are you the one we are looking for? Have you been working
-                            in recruitment and hiring for quite some time and are ready to
-                            further develop your skills? We are constantly searching for
-                            talented recruiters who are eager to grow.
+                            Excited to meet you! Are you the missing puzzle piece
+                            we've been searching for? If you're seasoned in the art of
+                            recruitment and hungry for even more growth, then look no
+                            further. Join our quest for talent as we continuously seek
+                            out passionate recruiters like you, poised to elevate their
+                            skills to new heights.
                         </p>
                         <p className="  mt-[2%] ">
-                            Alternatively, are you already at the top of your game and seeking
-                            an exciting new position? We have just what you need.
+                            Are you a master of your craft, seeking the next thrilling challenge?
+                            Look no further, for we have the perfect opportunity waiting for you.
+                            Dive into an exciting new role with us, where your expertise will be
+                            valued and your potential unleashed.
                         </p>
                         <div className="mt-[10%]">
                             <button className="text-[18px] font-bold bg-[#4353FF] hover:text-blue-500 hover:bg-white px-[22px] py-[15px] lg:px-[35px] lg:py-[22px]">
@@ -34,10 +31,6 @@ export default function Levelup() {
                     </div>
                 </div>
             </div>
-            {/* <div className="w-full overflow-visible">
-                <img className="w-full  transform -translate-x-1/2" src="https://www.wearekeen.com/hubfs/WeAreKeen_May_2021/images/europe-desktop.svg"
-                    alt="hr Image" />
-            </div> */}
         </div>
 
     )
