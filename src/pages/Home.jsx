6e1ac82415e8c9ac65a8bc619272e5135965f88a
Sai Kumar <43190921+saikumar1767@users.noreply.github.com>
2024-01-29 20:19:58 -0800

    recruitment_page commit (#7)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,15 @@ import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Levelup from "../components/Levelup";
 import Nutshell from "../components/Nutshell";
+import Services from  "../components/Services";
+import Companies from "../components/Companies";
 
 export default function Home() {
     return (
         <>
             <Hero />
+            <Companies />
+            <Services />
             <Nutshell />
             <Levelup />
             <Footer />
