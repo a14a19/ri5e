97aeb200e62b4c8b4e6c6b9541f13612ca282a3c
a14a19 <anas31197@gmail.com>
2024-01-31 15:17:46 +0530

    timeline added

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,14 +2,16 @@ import Bestrecruit from "../components/Bestrecruit";
 import Offices from "../components/Offices";
 import Stand from "../components/Stand";
 import WorkTogether from "../components/WorkTogether";
+import VerticalTimelineComp from "../subComponents/VerticalTimelineComp";
 
 export default function AboutUs() {
     return (
         <>
-        <Bestrecruit />
-        <Stand />
-        <Offices />
-        <WorkTogether />
+            <Bestrecruit />
+            <Stand />
+            <VerticalTimelineComp />
+            <Offices />
+            <WorkTogether />
         </>
     )
 }
---
+++ src/subComponents/VerticalTimelineComp.jsx
@@ -0,0 +1,52 @@
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+export default function VerticalTimelineComp() {
+  return (
+    <div className='bg-black px-5 py-28'>
+      <h2 className='md:text-5xl text-3xl font-bold mb-20 px-10'>Our Story</h2>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="2024 - present"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="2023 - 2024"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date="2022 - 2023"
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+        >
+          <h3 className="vertical-timeline-element-title">Creative Director</h3>
+          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+          </p>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  )
+}
+
